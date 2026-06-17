c4cea1eaa70a32730be0066ec7d0614d49c83f91
Hariom Mishra <mhariom014@gmail.com>
2026-06-17 14:12:49 +0530

    product model changes

--- app/models/product.py
+++ app/models/product.py
@@ -1,6 +1,6 @@
 from core.db import Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import String
+from sqlalchemy import String, Float, Integer
 from typing import List
 
 class Product(Base):
@@ -9,4 +9,9 @@ class Product(Base):
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     name: Mapped[str] = mapped_column(String(255), nullable=False)
     images: Mapped[List["Image"]] = relationship(back_populates="product")
-
+    price: Mapped[float] = mapped_column(Float, nullable=False)
+    special_price: Mapped[float] = mapped_column(Float, nullable=True)
+    stock: Mapped[int] = mapped_column(Integer, nullable=False)
+    category: Mapped[str] = mapped_column(String(255), nullable=False)
+    description: Mapped[str] = mapped_column(String(255), nullable=False)
+    
